e49a5893ab10bd331091fc20a18e8e233277b42f
singamreddyanitha <singamreddyanitha123@gmail.com>
2024-04-26 12:46:47 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:4000", {
+            const socket = io("https://chat-app-dtwh.onrender.com", {
                 query: {
                     userId: authUser._id,
                 }
