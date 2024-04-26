03a16c833c2465f9e161885166f05dde0eade10f
singamreddyanitha <singamreddyanitha123@gmail.com>
2024-04-26 15:03:05 +0530

    message updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 
 export const SocketContext = createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useSocketContext = () => {
     return useContext(SocketContext);
 };
